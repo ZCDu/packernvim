b56304bf1dc0374d81c361b2677ec20c762a90c2
3090 <my_server@edu.cn>
2022-08-13 22:24:55 +0800

    fix(alpha): project is not work, delete this item

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -24,7 +24,7 @@ dashboard.section.header.val = {
 dashboard.section.buttons.val = {
   button("f", icons.documents.Files .. " Find file", ":Telescope find_files <CR>"),
   button("e", icons.ui.NewFile .. " New file", ":ene <BAR> startinsert <CR>"),
-  button("p", icons.git.Repo .. " Find project", ":lua require('telescope').extensions.projects.projects()<CR>"),
+  -- button("p", icons.git.Repo .. " Find project", ":lua require('telescope').extensions.projects.projects()<CR>"),
   button("r", icons.ui.History .. " Recent files", ":Telescope oldfiles <CR>"),
   button("t", icons.ui.List .. " Find text", ":Telescope live_grep <CR>"),
   -- dashboard.button("s", icons.ui.SignIn .. " Find Session", ":silent Autosession search <CR>"),
